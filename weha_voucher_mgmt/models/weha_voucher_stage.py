4b0acabf259267e08b6eb7e259457b7c15ecb470
Wahyu Hidayat <wahhid@gmail.com>
2020-07-15 21:01:55 +0700

    Add Voucher Request Stage Module

--- weha_voucher_mgmt/models/weha_voucher_stage.py
+++ weha_voucher_mgmt/models/weha_voucher_stage.py
@@ -43,3 +43,84 @@ class WehaVoucherOrderStage(models.Model):
         default=lambda self: self.env['res.company']._company_default_get(
             'weha.voucher.order')
     )
+
+
+class WehaVoucherRequestStage(models.Model):
+    _name = 'weha.voucher.request.stage'
+    _description = 'Voucher Request Stage'
+    _order = 'sequence, id'
+    
+    name = fields.Char(string='Stage Name', required=True, translate=True)
+    description = fields.Text(translate=True)
+    sequence = fields.Integer(default=1)
+    active = fields.Boolean(default=True)
+    unattended = fields.Boolean(
+        string='Request')
+    l1 = fields.Boolean(
+        string='Level 1')
+    l2 = fields.Boolean(
+        string='Level 2')
+    opened = fields.Boolean(
+        string='Open')
+    closed = fields.Boolean(
+        string='Closed')
+    fold = fields.Boolean(
+        string='Folded in Kanban',
+        help="This stage is folded in the kanban view "
+             "when there are no records in that stage "
+             "to display.")
+    next_stage_id = fields.Many2one('weha.voucher.order.stage', 'Next Stage', required=False)
+    mail_template_id = fields.Many2one(
+        'mail.template',
+        string='Email Template',
+        domain=[('model', '=', 'weha.voucher.order')],
+        help="If set an email will be sent to the "
+             "customer when the ticket"
+             "reaches this step.")
+    company_id = fields.Many2one(
+        'res.company',
+        string="Company",
+        default=lambda self: self.env['res.company']._company_default_get(
+            'weha.voucher.order')
+    )
+
+class WehaVoucherReturnStage(models.Model):
+    _name = 'weha.voucher.return.stage'
+    _description = 'Voucher Return Stage'
+    _order = 'sequence, id'
+    
+    name = fields.Char(string='Stage Name', required=True, translate=True)
+    description = fields.Text(translate=True)
+    sequence = fields.Integer(default=1)
+    active = fields.Boolean(default=True)
+    unattended = fields.Boolean(
+        string='Request')
+    l1 = fields.Boolean(
+        string='Level 1')
+    l2 = fields.Boolean(
+        string='Level 2')
+    opened = fields.Boolean(
+        string='Open')
+    closed = fields.Boolean(
+        string='Closed')
+    fold = fields.Boolean(
+        string='Folded in Kanban',
+        help="This stage is folded in the kanban view "
+             "when there are no records in that stage "
+             "to display.")
+    next_stage_id = fields.Many2one('weha.voucher.order.stage', 'Next Stage', required=False)
+    mail_template_id = fields.Many2one(
+        'mail.template',
+        string='Email Template',
+        domain=[('model', '=', 'weha.voucher.order')],
+        help="If set an email will be sent to the "
+             "customer when the ticket"
+             "reaches this step.")
+    company_id = fields.Many2one(
+        'res.company',
+        string="Company",
+        default=lambda self: self.env['res.company']._company_default_get(
+            'weha.voucher.order')
+    )
+
+    
