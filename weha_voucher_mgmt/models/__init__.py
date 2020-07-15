6a323d79e2ff157aa6fd666da40bccd2c9bb0924
Wahyu Hidayat <wahhid@gmail.com>
2020-07-15 19:47:43 +0700

    Add weha_voucher_line on __init__.py

--- weha_voucher_mgmt/models/__init__.py
+++ weha_voucher_mgmt/models/__init__.py
@@ -1,5 +1,6 @@
 from . import weha_voucher_stage
 from . import weha_voucher_code
 from . import weha_voucher_order
+from . import weha_voucher_line
 from . import operating_unit
 from . import res_config_settings
