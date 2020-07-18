d28a434cb3ee76bfd11fd77f79189f6814263837
Yogi Abdul Yusuf <yogieabdulyusuf@gmail.com>
2020-07-18 09:05:02 +0700

    modified:   __manifest__.py (#3)
    modified:   data/weha_voucher_data.xml
    	modified:   models/__init__.py
    	deleted:    models/res_config_setttings.py
    	deleted:    models/weha_voucher.py
    	modified:   models/weha_voucher_order.py
    	modified:   models/weha_voucher_request.py
    	modified:   models/weha_voucher_stage.py
    	modified:   security/ir.model.access.csv
    	modified:   security/weha_voucher_security.xml
    	modified:   views/weha_voucher_dashboard_view.xml
    	modified:   views/weha_voucher_order_view.xml
    	modified:   views/weha_voucher_stage_view.xml
    
    Co-authored-by: wahhid <wahhid@gmail.com>

--- weha_voucher_mgmt/__manifest__.py
+++ weha_voucher_mgmt/__manifest__.py
@@ -15,6 +15,10 @@
         'views/weha_voucher_stage_view.xml',
         'views/weha_voucher_code_view.xml',
         'views/weha_voucher_order_view.xml',
+        'views/weha_voucher_config_location.xml',
+        'views/weha_voucher_config_number_range.xml',
+        'views/weha_voucher_config_terms.xml',
+        'views/weha_voucher_config_type.xml',
         'views/weha_voucher_dashboard_view.xml',
         'views/res_config_settings_view.xml',
         
