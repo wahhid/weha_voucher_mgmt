48fa5b4b78d77dae9bdeabe83cf43c5842c2dacc
Yogi AY <yogieabdulyusuf@gmail.com>
2020-07-18 20:13:23 +0700

    update init

--- weha_voucher_mgmt/models/__init__.py
+++ weha_voucher_mgmt/models/__init__.py
@@ -3,9 +3,6 @@ from . import weha_voucher_code
 from . import weha_voucher_order
 from . import weha_voucher_line
 from . import weha_voucher_config
-<<<<<<< HEAD
-=======
 from . import weha_voucher_request
->>>>>>> yogi
 from . import operating_unit
 from . import res_config_settings
