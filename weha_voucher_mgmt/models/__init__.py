8036957a559f1f3e0f5119e19eb6d939f77cac4b
Wahyu Hidayat <wahhid@gmail.com>
2020-07-19 10:37:48 +0700

    Change __init__.py

--- weha_voucher_mgmt/models/__init__.py
+++ weha_voucher_mgmt/models/__init__.py
@@ -3,4 +3,4 @@ from . import weha_voucher_code
 from . import weha_voucher_order
 from . import weha_voucher_line
 from . import operating_unit
-from . import res_config_settings
+from . import res_config_setting
